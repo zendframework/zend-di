731cffa751d209e48e24b0848f8c01ac3f5e94cc
Marco Pivetta <ocramius@gmail.com>
2015-01-11 17:48:29 +0000

    Importing state as of zendframework/zf2@d99f5c90927e71cb3cbfeeca10d27d79942c21c0 (1420998509)
    Automatic import via rsync

--- Definition/RuntimeDefinition.php
+++ Definition/RuntimeDefinition.php
@@ -3,7 +3,7 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
@@ -18,6 +18,7 @@ use Zend\Di\Di;
  */
 class RuntimeDefinition implements DefinitionInterface
 {
+
     /**
      * @var array
      */
@@ -176,8 +177,6 @@ class RuntimeDefinition implements DefinitionInterface
 
     /**
      * @param string $class
-     *
-     * @return bool
      */
     protected function hasProcessedClass($class)
     {
@@ -245,6 +244,7 @@ class RuntimeDefinition implements DefinitionInterface
         }
 
         foreach ($rClass->getMethods(Reflection\MethodReflection::IS_PUBLIC) as $rMethod) {
+
             $methodName = $rMethod->getName();
 
             if ($rMethod->getName() === '__construct' || $rMethod->isStatic()) {
@@ -256,6 +256,7 @@ class RuntimeDefinition implements DefinitionInterface
 
                 if (($annotations instanceof AnnotationCollection)
                     && $annotations->hasAnnotation('Zend\Di\Definition\Annotation\Inject')) {
+
                     // use '@inject' and search for parameters
                     $def['methods'][$methodName] = Di::METHOD_IS_EAGER;
                     $this->processParams($def, $rClass, $rMethod);
@@ -279,6 +280,7 @@ class RuntimeDefinition implements DefinitionInterface
             // by annotation
             // by setter pattern,
             // by interface
+
         }
 
         $interfaceInjectorPatterns = $this->introspectionStrategy->getInterfaceInjectionInclusionPatterns();
@@ -322,6 +324,7 @@ class RuntimeDefinition implements DefinitionInterface
         $def['parameters'][$methodName] = array();
 
         foreach ($rMethod->getParameters() as $p) {
+
             /** @var $p \ReflectionParameter  */
             $actualParamName = $p->getName();
 
@@ -335,5 +338,6 @@ class RuntimeDefinition implements DefinitionInterface
             $def['parameters'][$methodName][$fqName][] = !($optional = $p->isOptional() && $p->isDefaultValueAvailable());
             $def['parameters'][$methodName][$fqName][] = $optional ? $p->getDefaultValue() : null;
         }
+
     }
 }
